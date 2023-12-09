933437be0b34619273faa3a62863a62474cc08ec
Xavi Alfaro <nxb.developer@gmail.com>
2023-12-09 12:10:57 -0500

    update files after run prettier

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,35 +1,34 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json')
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo", "plugin:@typescript-eslint/recommended"],
-  env: {
-    node: true,
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
-  ],
-  overrides: [
-    {
-      files: ["*.js?(x)", "*.ts?(x)"],
-    },
-  ],
-  rules: {
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "jsx-quotes": "off",
-    "no-duplicate-imports": "error",
-  },
-};
+ extends: [
+  'eslint:recommended',
+  'prettier',
+  'eslint-config-turbo',
+  'plugin:@typescript-eslint/recommended'
+ ],
+ env: {
+  node: true
+ },
+ settings: {
+  'import/resolver': {
+   typescript: {
+    project
+   }
+  }
+ },
+ ignorePatterns: [
+  // Ignore dotfiles
+  '.*.js',
+  'node_modules/',
+  'dist/'
+ ],
+ overrides: [
+  {
+   files: ['*.js?(x)', '*.ts?(x)']
+  }
+ ]
+}
