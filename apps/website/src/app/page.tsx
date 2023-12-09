15b23885162dca7ac532e5f20a8dd094682d3dff
Xavi Alfaro <nxb.developer@gmail.com>
2023-12-09 12:55:01 -0500

    add shadcn

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Hellow world
+      <Button variant='outline'>Click me</Button>
     </main>
   )
 }
