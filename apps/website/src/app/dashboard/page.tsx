7c26ca8de6ff009618d33f53f49a4701929b05a3
Xavi Alfaro <nxb.developer@gmail.com>
2023-12-10 16:45:07 -0500

    add ui improvements

--- apps/website/src/app/dashboard/page.tsx
+++ apps/website/src/app/dashboard/page.tsx
@@ -21,65 +21,67 @@ export default async function Dashboard() {
 
   return (
     <>
-      <header className='px-4 border-b'>
-        <div className='flex h-12 items-center'>
-          <Link
-            href='/'
-            className='font-semibold sm:text-2xl text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-violet-700 dark:from-blue-300 dark:to-violet-400'
-          >
-            dbac
-          </Link>
-          <div className='ml-auto flex items-center space-x-4'>
-            <UserNavbar
-              avatar_url={avatar_url}
-              full_name={full_name}
-              user_name={user_name}
-            />
-          </div>
-        </div>
-      </header>
-      <div className='flex flex-1 overflow-hidden'>
-        <aside className='w-64 border-r border-zinc-200 dark:border-zinc-800 overflow-auto'>
-          <nav className='h-full flex flex-col gap-4 p-4'>
-            <div className='flex gap-2 items-center text-zinc-500 dark:text-gray-300'>
-              <GalleryVertical className='w-5 h-5' />
-              <h2 className='font-semibold'>Generations</h2>
+      <div className='flex flex-col px-3 2xl:px-4 min-h-screen'>
+        <header className='border-b'>
+          <div className='flex h-12 items-center'>
+            <Link
+              href='/'
+              className='font-semibold sm:text-2xl text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-violet-700 dark:from-blue-300 dark:to-violet-400'
+            >
+              dbac
+            </Link>
+            <div className='ml-auto flex items-center space-x-4'>
+              <UserNavbar
+                avatar_url={avatar_url}
+                full_name={full_name}
+                user_name={user_name}
+              />
             </div>
-            {true ? (
-              <div className='w-full h-full flex flex-col items-center justify-center text-center'>
-                <ScrollText className='w-12 h-12' />
-                <h3 className='mt-4 text-lg font-semibold'>
-                  No generations added
-                </h3>
-                <p className='mb-4 mt-2 text-sm text-muted-foreground'>
-                  You have not added any generations.
-                </p>
+          </div>
+        </header>
+        <div className='w-full flex overflow-hidden h-[calc(100vh-50px)]'>
+          <aside className='w-64 border-r border-zinc-200 dark:border-zinc-800 overflow-auto'>
+            <nav className='h-full flex flex-col gap-4'>
+              <div className='flex gap-2 items-center text-zinc-500 dark:text-gray-300 py-2'>
+                <GalleryVertical className='w-5 h-5' />
+                <h2 className='font-semibold'>Generations</h2>
               </div>
-            ) : (
-              <div className='flex-1 flex flex-col-reverse overflow-auto no-scrollbar'>
-                <div className='flex flex-col gap-3'>
-                  <button className='flex w-full h-full shrink-0 z-10 cursor-pointer relative outline-none focus-visible:ring-1 focus:ring-gray-700 rounded-md min-h-[25px] min-w-[40px]'>
-                    <div className='w-full rounded-md border border-black dark:border-gray-500 overflow-hidden'>
-                      <div className='w-full h-full hover:opacity-100 opacity-80 transition-opacity duration-300 ease-in-out'>
-                        <Image
-                          alt='Thumbnail database diagram'
-                          loading='lazy'
-                          width='320'
-                          height='180'
-                          className='object-cover aspect-video'
-                          src='/photo.webp'
-                        />
-                      </div>
-                    </div>
-                  </button>
+              {true ? (
+                <div className='w-full h-full flex flex-col items-center justify-center text-center'>
+                  <ScrollText className='w-12 h-12' />
+                  <h3 className='mt-4 text-lg font-semibold'>
+                    No generations added
+                  </h3>
+                  <p className='mb-4 mt-2 text-sm text-muted-foreground'>
+                    You have not added any generations.
+                  </p>
                 </div>
-              </div>
-            )}
-          </nav>
-        </aside>
-        <main className='w-full p-2'>
-          <PanelGeneration />
-        </main>
+              ) : (
+                <div className='flex-1 flex flex-col-reverse overflow-auto no-scrollbar'>
+                  <div className='flex flex-col gap-3'>
+                    <button className='flex w-full h-full shrink-0 z-10 cursor-pointer relative outline-none focus-visible:ring-1 focus:ring-gray-700 rounded-md min-h-[25px] min-w-[40px]'>
+                      <div className='w-full rounded-md border border-black dark:border-gray-500 overflow-hidden'>
+                        <div className='w-full h-full hover:opacity-100 opacity-80 transition-opacity duration-300 ease-in-out'>
+                          <Image
+                            alt='Thumbnail database diagram'
+                            loading='lazy'
+                            width='320'
+                            height='180'
+                            className='object-cover aspect-video'
+                            src='/photo.webp'
+                          />
+                        </div>
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </nav>
+          </aside>
+          <main className='w-full h-full p-2 overflow-auto'>
+            <PanelGeneration />
+          </main>
+        </div>
       </div>
     </>
   )
--- apps/website/src/components/panel-generation.tsx
+++ apps/website/src/components/panel-generation.tsx
@@ -8,10 +8,10 @@ export function PanelGeneration() {
   const { generationId, complete, completion } = useGeneration()
 
   return (
-    <div className='flex flex-col gap-2 w-full'>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-3 w-full'>
+    <div className='flex flex-col gap-2 w-full h-full'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-3 w-full h-full'>
         <Dropzone complete={complete} />
-        <div className='flex flex-col gap-2 w-full'>
+        <div className='flex flex-col gap-2 w-full h-full'>
           <CommandBox generationId={generationId} />
           <CodeEditor code={completion} />
         </div>
