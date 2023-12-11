c98064629ccc585097a524141d37f0b6a29a7a73
Xavi Alfaro <nxb.developer@gmail.com>
2023-12-11 11:17:10 -0500

    update addGeneration uses now supabase server client

--- apps/website/src/services/generation.ts
+++ apps/website/src/services/generation.ts
@@ -1,12 +1,9 @@
-import { createSupabaseBrowserClient } from '@/db/supabase-client'
+import { createSupabaseServerClient } from '@/db/supabase-server'
 import { TablesInsert } from '@/types/supabase'
 
 let GenerationInsert: TablesInsert<'generations'>
 
 export const addGeneration = async (generation: typeof GenerationInsert) => {
-  const supabase = await createSupabaseBrowserClient()
-  const { data, error } = await supabase.from('generations').insert(generation)
-
-  if (error) return null
-  return data
+  const supabase = await createSupabaseServerClient()
+  return supabase.from('generations').insert(generation)
 }
