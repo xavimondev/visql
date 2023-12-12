b43dae76f162614f8c517cace19586d1acb5e1e7
Xavi Alfaro <nxb.developer@gmail.com>
2023-12-12 16:55:42 -0500

    add better way to deal with initialized supabase projects

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -6,48 +6,82 @@ import ora from 'ora'
 import { execa } from 'execa'
 import prompts from 'prompts'
 import * as z from 'zod'
+import { glob } from 'glob'
 import { logger } from '@/helpers/logger.js'
 import { getSql } from '@/helpers/get-sql.js'
 
 const addArgumentsSchema = z.object({
-  generationId: z.string()
+  generation: z.string().optional()
 })
 
 const promptSchema = z.object({
-  supabasePath: z.string()
+  path: z.string().trim().min(1)
 })
 
-// .option('-y, --yes', 'skip confirmation prompt.', false)
 export const add = new Command()
   .name('add')
-  .arguments('<generationId>')
+  .arguments('[generation]')
   .description('initialize your supabase project and add migrations')
-  .action(async (generationId) => {
+  .action(async (generation) => {
     try {
-      console.log(`Getting diagram from ${generationId} `)
-      addArgumentsSchema.parse({ generationId })
+      const options = addArgumentsSchema.parse({ generation })
+      let generationCode = options.generation
+      if (!options.generation) {
+        const { generation } = await prompts([
+          {
+            type: 'text',
+            name: 'generation',
+            message: 'Enter your generation code'
+          }
+        ])
+        generationCode = generation
+      }
+      if (!generationCode) {
+        logger.warn('No generation code provided. Exiting.')
+        process.exit(0)
+      }
       const cwd = path.resolve(process.cwd())
-      const config = await promptForConfig(cwd)
+      let defaultDirectory = `${cwd}/supabase`
 
-      const supabasePath = path.join(
-        cwd,
-        config.supabasePath === 'supabase' ? '' : config.supabasePath
-      )
-      if (!existsSync(supabasePath)) {
-        mkdirSync(supabasePath, {
+      const search = path.join(cwd, '**/supabase/config.toml')
+      const result = await glob(search)
+      const pathFound = result.at(0)
+      if (pathFound) {
+        defaultDirectory = path.dirname(pathFound)
+      }
+      const relativePath = path.relative(cwd, defaultDirectory)
+      const config = await promptForConfig(relativePath)
+      const pickedPath = config.path ?? relativePath
+      console.log(`Relative path: ${pickedPath}`)
+
+      if (!existsSync(pickedPath) && pickedPath !== 'supabase') {
+        mkdirSync(pickedPath, {
           recursive: true
         })
       }
-      const spinner = ora(`Initializing Supabase project...`).start()
-      // 1. Initialize supabase project
-      await execa('npx', ['supabase', 'init'], {
-        cwd: supabasePath
-      })
-      spinner.succeed()
+
+      if (!pathFound) {
+        const spinner = ora(`Initializing Supabase project...`).start()
+        // 1. Initialize supabase project
+        await execa('npx', ['supabase', 'init'], {
+          cwd: pickedPath === 'supabase' ? cwd : pickedPath
+        })
+        spinner.succeed()
+      }
 
       const migrationSpinner = ora(`Adding migrations...`).start()
-      // 2. Create a folder migration
-      const migrationPath = path.join(supabasePath, 'supabase', 'migrations')
+      // 2. Create a folder migration...TODO: Find a better way to do this
+      const migrationPath = path.join(
+        `${
+          pickedPath === 'supabase'
+            ? `${cwd}/supabase`
+            : pathFound
+              ? pickedPath
+              : `${pickedPath}/supabase`
+        }`,
+        'migrations'
+      )
+
       if (!existsSync(migrationPath)) {
         mkdirSync(migrationPath, {
           recursive: true
@@ -55,7 +89,7 @@ export const add = new Command()
       }
 
       // 3. Reading tables from database
-      const tables = await getSql(generationId)
+      const tables = await getSql(generation)
       // 4. Add tables sql to migration file
       const now = new Date()
       const formattedTimestamp = now
@@ -75,17 +109,14 @@ export const add = new Command()
     }
   })
 
-export const promptForConfig = async (cwd: string, skip = false) => {
+export const promptForConfig = async (defaultDirectory: string) => {
   const highlight = (text: string) => chalk.cyan(text)
-
   const options = await prompts([
     {
       type: 'text',
-      name: 'supabasePath',
-      message: `Configure the path for ${highlight(
-        'supabase configuration'
-      )} ?`,
-      initial: 'supabase' ?? 'supabase'
+      name: 'path',
+      message: `Where would you like to add your ${highlight('migrations')} ?`,
+      initial: defaultDirectory
     }
   ])
   const config = promptSchema.parse(options)
