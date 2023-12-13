04d1ba5145aa784c24ae7d1dbffc1ff793a7c614
Xavi Alfaro <nxb.developer@gmail.com>
2023-12-13 13:09:07 -0500

    update api cli

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,5 +1,2 @@
-// TODO: find better ways to do this
-const LOCAL_DOMAIN = 'http://localhost:3000'
-
-export const API_GENERATIONS_URL = '/api/uptash/list-generations?code='
-export const DOMAIN = process.env.API_DOMAIN ?? LOCAL_DOMAIN
+const URL = 'https://visql.vercel.app'
+export const API_GENERATIONS_URL = `${URL}/api/uptash/list-generations?code=`
--- packages/cli/src/helpers/get-sql.ts
+++ packages/cli/src/helpers/get-sql.ts
@@ -1,13 +1,11 @@
-import { API_GENERATIONS_URL, DOMAIN } from '@/constants.js'
+import { API_GENERATIONS_URL } from '@/constants.js'
 
 export const getSql = async ({
   generationCode
 }: {
   generationCode: string
 }) => {
-  const response = await fetch(
-    `${DOMAIN}${API_GENERATIONS_URL}${generationCode}`
-  )
+  const response = await fetch(`${API_GENERATIONS_URL}${generationCode}`)
   const dataApi = await response.json()
   return dataApi.data
 }
