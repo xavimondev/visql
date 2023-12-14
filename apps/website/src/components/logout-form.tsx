a0b7591deb6ff9661b6f3a36d9017f6b05813561
Xavi Alfaro <nxb.developer@gmail.com>
2023-12-14 17:48:34 -0500

    update styles

--- apps/website/src/components/logout-form.tsx
+++ apps/website/src/components/logout-form.tsx
@@ -12,7 +12,10 @@ export function LogoutForm() {
   return (
     <>
       <form action={logout}>
-        <Button variant='ghost' className='p-0 m-0 w-auto h-auto'>
+        <Button
+          variant='ghost'
+          className='p-0 m-0 w-auto h-auto bg-transparent hover:bg-transparent'
+        >
           <LogOut className='mr-2 h-4 w-4' />
           <span>Log out</span>
         </Button>
