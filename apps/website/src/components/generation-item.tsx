04a07235f7c6f328fdda28529eec0f42060e7d7f
Xavi Alfaro <nxb.developer@gmail.com>
2023-12-14 15:02:10 -0500

    add hover card on list of generations

--- apps/website/src/components/generation-item.tsx
+++ apps/website/src/components/generation-item.tsx
@@ -1,6 +1,12 @@
 'use client'
 import Image from 'next/image'
 import { useStore } from '@/store'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger
+} from '@/components/ui/hover-card'
+import { CommandBox } from '@/components/command-box'
 
 type GenerationProps = {
   id: string
@@ -18,29 +24,68 @@ export function GenerationItem({
   const setGenerationData = useStore((state) => state.setGenerationData)
 
   return (
-    <button
-      key={id}
-      className='flex w-full h-full z-10 cursor-pointer relative outline-none focus-visible:ring-1 focus:ring-gray-700 rounded-md min-h-[25px] min-w-[30px]'
-      onClick={() => {
-        setGenerationData({
-          diagram_url,
-          cmd_code,
-          sql_code
-        })
-      }}
-    >
-      <div className='w-full rounded-md border border-black dark:border-gray-500 overflow-hidden'>
-        <div className='w-full h-full hover:opacity-100 opacity-80 transition-opacity duration-300 ease-in-out'>
+    <HoverCard openDelay={300} closeDelay={0}>
+      <HoverCardTrigger asChild>
+        <button
+          key={id}
+          className='flex 
+          w-full 
+          h-full 
+          cursor-pointer 
+          relative 
+          outline-none 
+          rounded-md 
+          min-h-[25px] 
+          min-w-[30px] 
+          border 
+          border-orange-200 
+          dark:border-orange-300 
+          hover:border-orange-400 
+          hover:dark:border-orange-600'
+          onClick={() => {
+            setGenerationData({
+              diagram_url,
+              cmd_code,
+              sql_code
+            })
+          }}
+        >
+          <div className='w-full rounded-md overflow-hidden'>
+            <div className='w-full h-full hover:opacity-100 opacity-80 transition-opacity duration-300 ease-in-out'>
+              <Image
+                alt='Thumbnail database diagram'
+                loading='lazy'
+                width='300'
+                height='180'
+                className='object-cover aspect-video'
+                src={diagram_url}
+              />
+            </div>
+          </div>
+        </button>
+      </HoverCardTrigger>
+      <HoverCardContent
+        className='w-80 
+        h-full 
+        p-3 
+        bg-black 
+        border-0 
+        dark:border 
+        dark:border-orange-300/30'
+        align='center'
+        side='right'
+      >
+        <div className='flex flex-col gap-2'>
           <Image
-            alt='Thumbnail database diagram'
-            loading='lazy'
-            width='300'
-            height='180'
-            className='object-cover aspect-video'
             src={diagram_url}
+            alt='A database diagram'
+            width={400}
+            height={400}
+            className='object-cover aspect-video w-full h-full'
           />
+          <CommandBox commandCode={cmd_code} />
         </div>
-      </div>
-    </button>
+      </HoverCardContent>
+    </HoverCard>
   )
 }
