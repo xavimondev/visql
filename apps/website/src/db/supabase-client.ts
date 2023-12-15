36ea5ab5011d2d51b4e10afbd8825d76c5cb9e60
Xavi Alfaro <nxb.developer@gmail.com>
2023-12-14 19:56:15 -0500

    add types to client

--- apps/website/src/db/supabase-client.ts
+++ apps/website/src/db/supabase-client.ts
@@ -1,8 +1,9 @@
 'use client'
 import { createBrowserClient } from '@supabase/ssr'
+import { Database } from '@/types/supabase'
 
 export const createSupabaseBrowserClient = async () => {
-  return createBrowserClient(
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
